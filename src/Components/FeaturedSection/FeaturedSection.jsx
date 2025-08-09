e88271fe280f37a057d9d8480e526b1097ad01a5
Sayed Sheikh <sayedsheikh9@gmail.com>
2025-08-09 23:40:21 +0600

    alignment corrected

--- src/Components/FeaturedSection/FeaturedSection.jsx
+++ src/Components/FeaturedSection/FeaturedSection.jsx
@@ -49,7 +49,7 @@ const FeaturedSection = () => {
 
       <Link
         to="/availableFood"
-        className="relative inline-block px-4 py-2 font-medium group cursor-pointer my-10">
+        className="relative inline-block px-4 py-2 font-medium group cursor-pointer my-10 mb-0">
         <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-secondary/50 group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
         <span className="absolute inset-0 w-full h-full bg-base-100 border-2 border-secondary group-hover:bg-secondary"></span>
         <span
