4a8329594a601ccb848f1b998297bfbb6727a7b3
Sayed Sheikh <sayedsheikh9@gmail.com>
2025-08-09 23:33:01 +0600

    services section updated

--- src/Components/OurServicesSection/OurServicesSection.jsx
+++ src/Components/OurServicesSection/OurServicesSection.jsx
@@ -6,7 +6,7 @@ const OurServicesSection = () => {
     <section className="py-16 bg-base-100 text-base-content font-inter">
       <div className="max-w-[1200px] mx-auto px-4 text-center">
         <h2 className="text-3xl font-bold text-primary mb-4">Our Services</h2>
-        <p className="text-gray-500 mb-10 max-w-xl mx-auto">
+        <p className=" mb-10 max-w-xl mx-auto">
           We connect food donors with those in need to reduce waste and build a
           stronger, caring community.
         </p>
@@ -17,7 +17,7 @@ const OurServicesSection = () => {
               <FaHandHoldingHeart size={36} className="text-primary" />
             </div>
             <h3 className="text-xl font-semibold mb-2">Food Donation</h3>
-            <p className="text-sm text-gray-600">
+            <p className="text-sm ">
               Easily donate extra food and help those in need. We ensure secure
               and efficient food distribution.
             </p>
@@ -28,7 +28,7 @@ const OurServicesSection = () => {
               <FaRecycle size={36} className="text-primary" />
             </div>
             <h3 className="text-xl font-semibold mb-2">Zero Waste Goal</h3>
-            <p className="text-sm text-gray-600">
+            <p className="text-sm">
               Our mission is to reduce food waste by connecting people who have
               surplus food with those who need it.
             </p>
@@ -39,7 +39,7 @@ const OurServicesSection = () => {
               <FaUsers size={36} className="text-primary" />
             </div>
             <h3 className="text-xl font-semibold mb-2">Community Driven</h3>
-            <p className="text-sm text-gray-600">
+            <p className="text-sm ">
               Empowering communities to support each other with food sharing and
               compassion.
             </p>
